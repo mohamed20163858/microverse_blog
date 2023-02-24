d592999331bb36d5b42c2801f4dc743677ac75d5
mohamed saleh <mohamed20163858@gmail.com>
2023-02-24 15:51:57 +0200

    Fix rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  has_many :comments 
+  belongs_to :author, class_name: 'User'
+  has_many :comments
   has_many :likes
   after_save :update_posts_counter
 
-  def update_posts_counter 
+  def update_posts_counter
     author.increment!(:posts_counter)
-  end 
+  end
 
-  def recent_five_comments 
+  def recent_five_comments
     comments.order(created_at: :desc).first(5)
   end
 end
