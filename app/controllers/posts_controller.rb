130ef3fc85753f6cc55b45e4846174c3efde7fe7
Mohamed Saleh <mohamed20163858@gmail.com>
2023-03-05 01:57:42 +0200

    Add posts, comments, and like functionalities'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,23 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id].to_i)
+    @comment = Comment.new
+    @like = Like.new
+    @user = current_user
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.save
+    redirect_to "/users/#{current_user.id}/posts/#{@post.id}"
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index; end
 
   def show
-    @user = User.find_by(id: params[:user_id].to_i)
+    @user = User.find_by(id: params[:user_id].to_i) 
+    @current_user = current_user
   end
 end
