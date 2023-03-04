3564bbea7e885adb4ae7127c5b327e06f49abe40
Mohamed Saleh <mohamed20163858@gmail.com>
2023-03-05 00:01:31 +0200

    Fix rubocop and stylelint errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id: params[:user_id].to_i);
+    @user = User.find_by(id: params[:user_id].to_i)
   end
+
   def show
-    @post = Post.find_by(id: params[:id].to_i);
+    @post = Post.find_by(id: params[:id].to_i)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def index; end
 
   def show
-    @user = User.find_by(id: params[:user_id].to_i);
+    @user = User.find_by(id: params[:user_id].to_i)
   end
 end
